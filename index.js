f43dc4781c1e425337be576fb118bf873b4ddff0
karen-yquevedo <kyamilequevedo@gmail.com>
2022-04-29 14:09:17 +0200

    html & js.index

---
+++ index.js
@@ -0,0 +1,32 @@
+//1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avenger = avengers[0];
+console.log(avenger);
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers.unshift ("IRONMAN");
+console.log (avengers);
+
+
+//1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+alert(avengers);
+
+//1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+const rickAndMorty = rickAndMortyCharacters[4];
+console.log(rickAndMorty);
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+rickAndMorty = rickAndMortyCharacters[0, 4];
+console.log(rickAndMorty);
+
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.splice(1,1);
+console.log(rickAndMortyCharacters);
